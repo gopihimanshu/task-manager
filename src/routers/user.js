56476e345b852dc7495ead6ef5842f8dab0b0a5c
gopihimanshu <gopihimanshu@gmail.com>
2020-02-23 17:06:30 +0530

    Added schema defination

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,12 +3,7 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-router.get('/test', (req, res) => {
-    res.send('From New File')
-})
-
 router.post('/users', async (req, res) => {
-    console.log('Hello')
     const user = new User(req.body);
 
     try {
@@ -54,7 +49,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new : true, runValidators: true})
+        // const user = await User.findByIdAndUpdate(req.params.id, req.body, {new : true, runValidators: true})
+        const user = await User.findById(req.params.id);
+
+        updates.forEach( update => user[update] = req.body[update] );
+        await user.save();
 
         if (!user) {
             return res.status(404).send()
