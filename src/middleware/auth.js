3d4b6b043ea53a9534c580fee2d56ed5e15d74a3
gopihimanshu <gopihimanshu@gmail.com>
2020-02-27 08:44:31 +0530

    Added the user authentication

---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+
+    try {
+        const token = req.header('Authorization')
+        console.log(token);
+        
+    } catch (error) {
+        res.status(401).send({error: 'Please authenticate'})
+    }
+    next()
+}
+
+module.exports = auth;
